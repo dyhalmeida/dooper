f2c1a42c7a0cdbebb2f30661222ba0eeb3c850b7
Diego Almeida <dyhalmeida@gmail.com>
2020-05-12 11:49:45 -0300

    adicionado botão de criar produto

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -3,11 +3,12 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [],
-  imports: [MatToolbarModule, MatSidenavModule, MatListModule, MatCardModule],
-  exports: [MatToolbarModule, MatSidenavModule, MatListModule, MatCardModule],
+  imports: [MatToolbarModule, MatSidenavModule, MatListModule, MatCardModule, MatButtonModule],
+  exports: [MatToolbarModule, MatSidenavModule, MatListModule, MatCardModule, MatButtonModule],
   providers: []
 })
 export class MaterialdModule {}
--- src/app/views/products/products.module.ts
+++ src/app/views/products/products.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
+import { SharedModule } from 'src/app/shared/shared.module';
 
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './products.component';
+import { ProductCreateComponent } from './components/product-create/product-create.component';
 
 
 @NgModule({
-  declarations: [ProductsComponent],
+  declarations: [ProductsComponent, ProductCreateComponent],
   imports: [
-    CommonModule,
+    SharedModule,
     ProductsRoutingModule
   ]
 })
