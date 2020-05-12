a5e4eb43a8e038b0b8e9fa7203bffa1cf9f05a3d
Diego Almeida <dyhalmeida@gmail.com>
2020-05-12 10:40:34 -0300

    Adiciona módulo home e sua página

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav'
-import { MatListModule } from '@angular/material/list'
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [],
-  imports: [MatToolbarModule, MatSidenavModule, MatListModule],
-  exports: [MatToolbarModule, MatSidenavModule, MatListModule],
+  imports: [MatToolbarModule, MatSidenavModule, MatListModule, MatCardModule],
+  exports: [MatToolbarModule, MatSidenavModule, MatListModule, MatCardModule],
   providers: []
 })
 export class MaterialdModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { MaterialdModule } from './material.module';
 
 @NgModule({
   declarations: [],
-  imports: [FormsModule, MaterialdModule],
-  exports: [FormsModule, MaterialdModule],
+  imports: [FormsModule, CommonModule, MaterialdModule],
+  exports: [FormsModule, CommonModule, MaterialdModule],
   providers: []
 })
 export class SharedModule {}
