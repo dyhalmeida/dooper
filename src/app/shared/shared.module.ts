dd4ebabbe7adc4d3bfa5a790c6238e7f1a1f97a4
Diego Almeida <dyhalmeida@gmail.com>
2020-05-12 12:26:13 -0300

    Adicionado HttpClientModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,26 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { MaterialdModule } from './material.module';
 import { DirectivesModule } from '../directives/directives.module';
 
 @NgModule({
   declarations: [],
-  imports: [FormsModule, CommonModule, DirectivesModule, MaterialdModule],
-  exports: [FormsModule, CommonModule, DirectivesModule, MaterialdModule],
+  imports: [
+    FormsModule,
+    HttpClientModule,
+    CommonModule,
+    DirectivesModule,
+    MaterialdModule,
+  ],
+  exports: [
+    FormsModule,
+    HttpClientModule,
+    CommonModule,
+    DirectivesModule,
+    MaterialdModule,
+  ],
   providers: []
 })
 export class SharedModule {}
